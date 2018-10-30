4efb68638fe89fced164b1ed423d0659ec168a07
Ioannis Nikas <ioa.nikas@gmail.com>
2018-10-30 22:23:54 +0100

    style et nom des bonbons

--- src/app/list-bonbons/list-bonbons.component.ts
+++ src/app/list-bonbons/list-bonbons.component.ts
@@ -8,78 +8,92 @@ import { Component, OnInit } from '@angular/core';
 export class ListBonbonsComponent implements OnInit {
 
   candyList : object[] = [
-    { name : "Bonbon 1",
+    { name : "Dragibeurk",
       quality : "Bonbon commun",
       description : "Le plus commun des bonbons communs, le cauchemar des parents, le rêve des dentistes !",
       quantity : 0, 
       img : 'https://image.ibb.co/bZzzL0/Candy1A.png'},
 
-      { name : "Bonbon 2",
+      { name : "Candycrusher",
         quality : "Bonbon commun",
-        description : "Véritable casseur de dents en série, la réputation de Bonbon 2 n'est plus à faire !",
-        quantity : 0, 
-        img : 'https://image.ibb.co/mDzPL0/Candy2.png'},
-
-      { name : "Bonbon 3",
-        quality : "Bonbon commun",
-        description : "Bonbon 3 aurait pu gagner en prestige... si seulement il n'était pas aromatisé à la réglisse !",
-        quantity : 0,
-        img : 'https://image.ibb.co/c4OjL0/Candy3.png' },
-
-      { name : "Bonbon 4",
-        quality : "Bonbon commun",
-        description : "Les gens méchants mettent des raisins dans leurs muffins, d'autres y mettent des Bonbon 4...",
+        description : "Véritable casseur de dents en série, la réputation du Candycrusher n'est plus à faire !",
         quantity : 0, 
         img : 'https://image.ibb.co/jB5aSf/Candy4.png'},
 
-      { name : "Bonbon 5",
+      { name : "Ratella",
         quality : "Bonbon commun",
-        description : "Un peu acide, un peu amer, Bonbon 5 n'a décidément rien pour plaire.",
+        description : "Ratella aurait pu gagner en prestige... si seulement il n'était pas aromatisé à la réglisse !",
+        quantity : 0,
+        img : 'https://image.ibb.co/c4OjL0/Candy3.png' },
+
+      { name : "Bofbof",
+        quality : "Bonbon commun",
+        description : "Les gens méchants mettent des raisins dans leurs muffins, d'autres y mettent des Bofbofs...",
+        quantity : 0, 
+        img : 'https://image.ibb.co/mDzPL0/Candy2.png'},
+
+      { name : "Pomme d'amer",
+        quality : "Bonbon commun",
+        description : "Un peu acide, un peu amer, la Pomme d'amer n'a décidément rien pour plaire.",
         quantity : 0, 
         img : 'https://image.ibb.co/evUPL0/Candy5.png'},
 
-      { name : "Bonbon 6",
+      { name : "Berlingus",
         quality : "Bonbon rare",
-        description : "C'est de lui dont nous parlaient nos parent lorsqu'ils étaient gamins : Bonbon 8 vous laissera des souvenirs impérissables",
+        description : "C'est de lui dont nous parlaient nos parent lorsqu'ils étaient gamins : le Berlingus vous laissera des souvenirs impérissables",
         quantity : 0, 
-        img : 'https://image.ibb.co/ewGh7f/Candy6.png'},
+        img : 'https://image.ibb.co/iSALtL/Candy7.png'}, 
 
-      { name : "Bonbon 7",
+      { name : "Gonegone",
         quality : "Bonbon rare",
         description : "De son petit surnom le \"bonbec des gones\", il est aussi savoureux qu'une rosette fraichement tranchée.",
         quantity : 0, 
-        img : 'https://image.ibb.co/iSALtL/Candy7.png'},
+        img : 'https://image.ibb.co/ewGh7f/Candy6.png'},
 
-      { name : "Bonbon 8",
+      { name : "Chocodude",
         quality : "Bonbon rare",
-        description : "Bonbon 8 n'a jamais réussi à être accepté par ses camarades bonbons. C'est peut-être ça qui l'a rendu aussi méchant...",
+        description : "Le Chocodude n'a jamais réussi à être accepté par ses camarades bonbons. C'est peut-être parce que c'est une barre de chocolat...",
         quantity : 0, 
         img : 'https://image.ibb.co/eUkmYL/Candy8.png'},
 
-      { name : "Bonbon 9",
+      { name : "Coolcake",
         quality : "Bonbon rare",
-        description : "Quand y'en a marre, y a Bonbon 9 !",
+        description : "Quand y'en a marre, y a Coolcake !",
         quantity : 0, 
         img : 'https://image.ibb.co/k91RYL/Candy9.png'},
 
-      { name : "Bonbon 10",
+      { name : "Boulmagik",
         quality : "Bonbon épique",
-        description : "Lorsqu'on parle de Serious Candiness, on évoque plus qu'à demi-mot Bonbon 10",
+        description : "Lorsqu'on parle de Serious Candiness, on évoque plus qu'à demi-mot la Boulmagik",
         quantity : 0, 
         img : 'https://image.ibb.co/gBbtDL/Candy10.png'},
 
-      { name : "Bonbon 11",
+      { name : "Carambourre",
         quality : "Bonbon épique",
-        description : "Véritable caviar parmi les innombrables douceurs accessibles à nos palais, Bonbon 11 est de ceux qu'on ne mange qu'une fois dans une vie.",
+        description : "Véritable caviar parmi les innombrables douceurs accessibles à nos palais, le Carambourre est de ceux qu'on ne mange qu'une fois dans une vie.",
         quantity : 0, 
         img : 'https://image.ibb.co/iFqaSf/Candy11.png'},
 
-      { name : "Bonbon 12",
+      { name : "Harib'oeil",
         quality : "Bonbon légendaire",
         description : "La légendaire raconte qu'un légendaire bonbon n'attendrait qu'un dresseur de bonbons assez brave pour le croquer. Et de la bravoure, il en faut pour avaler ce que l'on croit être un oeil !",
         quantity : 0, 
         img : 'https://image.ibb.co/nzNpnf/Candy12.png'}
   ]
+
+  getColor(quality) { 
+    switch (quality) {
+      case 'Bonbon commun':
+        return 'white';
+      case 'Bonbon rare':
+        return 'blue';
+      case 'Bonbon épique':
+        return 'purple';
+      case 'Bonbon légendaire':
+        return 'orange';
+        
+    }
+  }
 
   constructor() { }
 
