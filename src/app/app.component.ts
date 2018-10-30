3a83e4d03c931f058fe46b923528e45f022fdb5d
Eliott R <eliott.raillere@gmail.com>
2018-10-30 16:10:29 +0100

    pour mon bro florent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,17 @@ export class AppComponent {
   title = 'CandyGo';
   myLng;
   myLat;
+
+  
   enablePlayMode=false;
 displayLocationInfo = (position) => {
   this.myLng = position.coords.longitude;
   this.myLat = position.coords.latitude;
   this.enablePlayMode=true;
+  this.myLng.toFixed(6);
+  this.myLat.toFixed(6);
 }
+
 
 ngOnInit() {
   if (navigator.geolocation) {
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,8 +11,25 @@ export class MapComponent implements OnInit {
   @Input() myLat;
 
 
-  lat = this.myLng;
-  lng = this.myLat;
+  randomLng;
+  randomLat;
+
+  setRandomLngFromGeo(){
+    console.log(this.myLng)
+  }
+  setRandomLatFromGeo(){
+    this.randomLat=this.myLat+this.randomLatAdder();
+    return this.randomLat;
+  }
+
+  randomLatAdder = () => { // Génère de façon aléatoire une latitude positive ou négative (définie entre -0.005 et 0.005).
+    return Math.floor(Math.random() * 5 + 2) / 1000;
+    };
+    
+  randomLonAdder = () => { // Génère de façon aléatoire une longitude positive ou négative (définie entre -0.008 et 0.008).
+    return Math.floor(Math.random() * 8 + 2) / 1000;
+    }; 
+  
 
   constructor() { }
   ngOnInit() {
